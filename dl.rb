bec3624e79a31f5d0b769a36ea505719bbbf9109
ritiek <ritiekmalhotra123@gmail.com>
2018-02-17 16:47:37 +0530

    Basic implementation

--- dl.rb
+++ dl.rb
@@ -10,41 +10,59 @@ def get_gist_url(name)
 end
 
 
-def get_gist_code(gist_url)
+def gist_code(gist_url)
     gist = Net::HTTP.get_response(URI(gist_url))
     code = gist.body
     return code
 end
 
 
-def download_code(name)
-    gist_url = get_gist_url(name)
-    filename =  gist_url.split('/').last
+def create_gist(name, code)
+    uri = URI('https://api.github.com/gists')
+    payload = {
+      'files' => {
+        name => {
+          'content' => code
+        }
+      }
+    }
 
-    File.open(filename, 'w') { |file| file.write(code) }
-    return filename
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = payload.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+      http.request(req)
+    end
+
+    raw_url = JSON.parse(res.body)['files'][name]['raw_url']
+    return raw_url
 end
 
-## Uploading Code
 
-# name = 'dl.rb'
-# code = File.open(name, 'rb').read
-# uri = URI('https://api.github.com/gists')
-# payload = {
-#   'files' => {
-#     name => {
-#       'content' => code
-#     }
-#   }
-# }
-# req = Net::HTTP::Post.new(uri.path)
-# req.body = payload.to_json
-# res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
-#   http.request(req)
-# end
-# puts res.body.inspect
+def shorten_url(github_url, code)
+    uri = URI('https://git.io')
+    response = Net::HTTP.post_form(uri, 'url' => github_url, 'code' => code)
+    return response
+end
 
-# uri = URI('https://git.io')
-# res = Net::HTTP.post_form(uri, 'url' => 'https://github.com/thisissocold', 'code' => name)
-# puts res.code
-# puts res['location']
+
+type = ARGV[0]
+short_name = ARGV[1]
+
+if ARGV.count < 2
+    puts "usage: #{$0} [-d | -u] <unique_identifier>"
+    abort
+end
+
+if type == '-d'
+    url = get_gist_url(short_name)
+    code = gist_code(url)
+    Clipboard.copy(code)
+    puts 'copied from ' + url
+elsif type == '-u'
+    raw_url = create_gist(short_name, Clipboard.paste())
+    response = shorten_url(raw_url, short_name)
+    puts response.code
+    puts response['location']
+else
+    puts "usage: #{$0} [-d | -u] <unique_identifier>"
+end
