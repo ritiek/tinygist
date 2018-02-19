e6da1fb964cebaae1eb5291f924513c08a25ab63
ritiek <ritiekmalhotra123@gmail.com>
2018-02-19 12:14:26 +0530

    Some refactoring

--- test.go
+++ test.go
@@ -5,15 +5,14 @@ import (
     "net/http"
     "bytes"
     "io/ioutil"
-    "os"
+    //"os"
+    //"encoding/json"
     )
 
 func get_gist_contents(name string) string {
     response, err := http.Get("https://git.io/" + name)
     if err != nil {
-        fmt.Printf("%s", err)
-        os.Exit(1)
-        return "b"
+        panic(err)
     } else {
         defer response.Body.Close()
         contents, err := ioutil.ReadAll(response.Body)
@@ -24,30 +23,8 @@ func get_gist_contents(name string) string {
     }
 }
 
-
-/*  def create_gist(name, code)
-    uri = URI('https://api.github.com/gists')
-    payload = {
-      'public' => false,
-      'files' => {
-        name => {
-          'content' => code
-        }
-      }
-    }
-
-    req = Net::HTTP::Post.new(uri.path)
-    req.body = payload.to_json
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
-      http.request(req)
-    end
-
-    raw_url = JSON.parse(res.body)['files'][name]['raw_url']
-    return raw_url
-end*/
-
 func create_gist(name string, code string) string {
-    json := []byte(`{
+    payload := []byte(`{
             "public": "false",
             "files": {
                 "` + name + `": {
@@ -56,7 +33,8 @@ func create_gist(name string, code string) string {
                 }
             }`)
 
-    req, err := http.NewRequest("POST", "https://api.github.com/gists", bytes.NewBuffer(json))
+    req, err := http.NewRequest("POST", "https://api.github.com/gists",
+                                bytes.NewBuffer(payload))
     client := &http.Client{}
     resp, err := client.Do(req)
     if err != nil {
@@ -64,14 +42,16 @@ func create_gist(name string, code string) string {
     }
     defer resp.Body.Close()
     body, _ := ioutil.ReadAll(resp.Body)
+
     return string(body)
 }
+
 
 func main() {
     // name := "bla123321he"
     // contents := get_gist_contents(name)
     // fmt.Println(contents)
 
-	// contents := create_gist("bla123321he", "this is sparta")
-	// fmt.Println(contents)
+	raw_url := create_gist("bla123321he", "this is sparta")
+	fmt.Println(raw_url)
 }
