f2af08001c2ba0c011bc2675f8d23508f37211ac
ritiek <ritiekmalhotra123@gmail.com>
2018-02-19 14:00:36 +0530

    Return Location header

--- test.go
+++ test.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "net/url"
     "bytes"
     "io/ioutil"
     "github.com/tidwall/gjson"
@@ -47,12 +48,31 @@ func create_gist(name string, code string) string {
     return value.String()
 }
 
+func shorten_url(github_url string, code string) string {
+    form := url.Values{
+            "url"  : {github_url},
+            "code" : {code},
+    }
+	body := bytes.NewBufferString(form.Encode())
+	rsp, err := http.Post("https://git.io", "application/x-www-form-urlencoded", body)
+	if err != nil {
+		panic(err)
+	}
+	defer rsp.Body.Close()
+
+    return rsp.Header.Get("Location")
+}
+
 
 func main() {
-    // name := "bla123321he"
-    // contents := get_gist_contents(name)
-    // fmt.Println(contents)
+    name := "hello_crazy_"
+    raw_url := "https://gist.githubusercontent.com/anonymous/c7d4e1983288f318e3b732fce88bbd88/raw/df8c54286144c469de33ff7d75b5cefe8a9edf7d/hello_crazy_"
+	//raw_url := create_gist(name, "this is sparta! :)")
+	//fmt.Println(raw_url)
 
-	raw_url := create_gist("bla123321he", "this is sparta!")
-	fmt.Println(raw_url)
+    //contents := get_gist_contents(name)
+    //fmt.Println(contents)
+
+    github_url := shorten_url(raw_url, name)
+    fmt.Println(github_url)
 }
