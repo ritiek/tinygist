9ca01840833c9a5732772c0035af1bc9f612ebd7
ritiek <ritiekmalhotra123@gmail.com>
2018-02-19 19:44:44 +0530

    Follow style conventions

--- test.go
+++ test.go
@@ -1,64 +1,61 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "net/url"
-    "bytes"
-    "io/ioutil"
-    "flag"
-    "os"
-    "github.com/tidwall/gjson"
-    "github.com/atotto/clipboard"
-    )
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+)
 
-
-func get_gist_contents(name string) string {
-    response, err := http.Get("https://git.io/" + name)
-    if err != nil {
-        panic(err)
-    } else {
-        defer response.Body.Close()
-        contents, err := ioutil.ReadAll(response.Body)
-        if err != nil {
-            panic(err)
-        }
-        return string(contents)
-    }
+func getGistContents(identifier string) string {
+	response, err := http.Get("https://git.io/" + identifier)
+	if err != nil {
+		panic(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		return string(contents)
+	}
 }
 
-
-func create_gist(name string, code string) string {
-    payload := []byte(`{
+func createGist(identifier string, code string) string {
+	payload := []byte(`{
             "public": "false",
             "files": {
-                "` + name + `": {
+                "` + identifier + `": {
                     "content":"` + code + `"
                     }
                 }
             }`)
 
-    req, err := http.NewRequest("POST", "https://api.github.com/gists",
-                                bytes.NewBuffer(payload))
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
+	req, err := http.NewRequest("POST", "https://api.github.com/gists",
+		bytes.NewBuffer(payload))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-    value := gjson.Get(string(body), "files." + name + ".raw_url")
-    return value.String()
+	value := gjson.Get(string(body), "files." + identifier + ".raw_url")
+	return value.String()
 }
 
-
-func shorten_url(github_url string, code string) (string, int) {
-    form := url.Values {
-            "url"  : {github_url},
-            "code" : {code},
-    }
+func shortenURL(github_url string, code string) (string, int) {
+	form := url.Values{
+		"url":  {github_url},
+		"code": {code},
+	}
 	body := bytes.NewBufferString(form.Encode())
 	rsp, err := http.Post("https://git.io", "", body)
 	if err != nil {
@@ -66,40 +63,39 @@ func shorten_url(github_url string, code string) (string, int) {
 	}
 	defer rsp.Body.Close()
 
-    return rsp.Header.Get("Location"), rsp.StatusCode
+	return rsp.Header.Get("Location"), rsp.StatusCode
 }
 
-
 func main() {
-    download := flag.Bool("d", true,
-                        "Copy gist contents to clipboard")
+	downloadFlag := flag.Bool("d", true,
+		"Copy gist contents to clipboard")
 
-    upload := flag.Bool("u", false,
-                        "Create gist from clipboard content")
+	uploadFlag := flag.Bool("u", false,
+		"Create gist from clipboard content")
 
-    name := flag.String("i", "REQUIRED",
-                        "Unique Identifier")
+	identifier := flag.String("i", "REQUIRED",
+		"Unique Identifier")
 
-    flag.Parse()
+	flag.Parse()
 
-    if *name == "REQUIRED" {
-        fmt.Println("Please set a unique identifier: `-i <unique_identifier>`")
-        os.Exit(1)
-    }
+	if *identifier == "REQUIRED" {
+		fmt.Println("Please pass a unique identifier: `-i <uniqueIdentifier>`")
+		os.Exit(1)
+	}
 
-    if *upload {
-        content, _ := clipboard.ReadAll()
-        raw_url := create_gist(*name, content)
-        _, return_code := shorten_url(raw_url, *name)
-        if return_code >= 200 && return_code <= 299 {
-            fmt.Println(raw_url)
-        } else {
-            fmt.Println("Identifier already used up!")
-            fmt.Println("Try again with different unique identifer")
-        }
-    } else if *download {
-        content := get_gist_contents(*name)
-        clipboard.WriteAll(content)
-        fmt.Println(content)
-    }
+	if *uploadFlag {
+		content, _ := clipboard.ReadAll()
+		rawURL := createGist(*identifier, content)
+		_, returnCode := shortenURL(rawURL, *identifier)
+		if returnCode >= 200 && returnCode <= 299 {
+			fmt.Println(rawURL)
+		} else {
+			fmt.Println("Identifier already used up!")
+			fmt.Println("Try again with different unique identifer")
+		}
+	} else if *downloadFlag {
+		content := getGistContents(*identifier)
+		clipboard.WriteAll(content)
+		fmt.Println(content)
+	}
 }
